3b4ed9723cc8dd44d13debde7917f760a14496ab
Konrad <konrad.babowicz21@gmail.com>
2024-05-18 23:24:45 +0100

    created notes

---
+++ src/index.js
@@ -0,0 +1,24 @@
+//Go through what you need, create a checklist
+//When working with API's go through the previous project you made to display gifs
+//explain how you can use smaller functions like the below to make it easer and more understandable code
+//Learn how to implement weather API in to your code
+//research functions how to use them again
+
+//need to get some icons
+//start with simple
+// then move to harder functions
+// as an easy exercise to start
+// create a function, that can take a location and return weather data from that location
+//console log the information
+
+
+//learn how to implement loading, at the end.
+
+
+//functions like
+//getweather data
+//processData
+//display Data
+//fetch weather
+//display error
+//function to reset . resets form
